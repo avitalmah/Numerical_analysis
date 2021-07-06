f272114d349985f0e881fc608625f9017f3e2417
avitalmah <73911258+avitalmah@users.noreply.github.com>
2021-07-06 22:48:23 +0300

    Update Richardson.py

--- Research/Richardson.py
+++ Research/Richardson.py
@@ -3,6 +3,14 @@ from sympy.utilities.lambdify import lambdify
 import sympy as sp
 
 def Richardson(f, x0 , min_h , k ):
+    """
+    Function for ccalculating value approximation at a certin point.
+    :param f: The function.
+    :param x0: The point which to seek approximation.
+    :param min_h: The minimum step.
+    :param k: Error order of magnitude.
+    :return: The maximum approximation.
+    """
     t = 2
     h = min_h*t**(k-1)
     A = lambda step : centeral(f,x0,step)
@@ -10,10 +18,11 @@ def Richardson(f, x0 , min_h , k ):
     while h >= min_h:
         A_list.append(A(h))
         print("h:"+str(h))
-        print("A:" + str(A_list[-1]))
+        print("f'(x):" + str(A_list[-1]))
         h/=t
     j = 2
     while (len(A_list) != 1):
+        print ("Iter num :"+str(j-1))
         print(A_list)
         A_list = inter(A_list,j)
         j += 1
@@ -21,6 +30,12 @@ def Richardson(f, x0 , min_h , k ):
     return A_list[0]
 
 def inter(A_list,j):
+    """
+    Iterative function for calculation the next step iter.
+    :param A_list: List of derivative in scale k.
+    :param j: Num of the iter.
+    :return: Next list of the next iter derivative.
+    """
     next_A=[]
     for i in range (len(A_list)-1):
         a = A_list[i]
@@ -30,6 +45,13 @@ def inter(A_list,j):
     return next_A
 
 def centeral(f,x0,h):
+    """
+    The central derivative function
+    :param f: The function whose derivative is calculated at x0.
+    :param x0: The point which searches is derived.
+    :param h: The step.
+    :return: Derived at point x with step h.
+    """
     return (f(x0+h)-f(x0-h))/(2*h)
 
 def sin(x):
@@ -45,13 +67,11 @@ def main():
 
     f = (input("enter function of x: "))
     x = sp.symbols('x')
-
     f = lambdify(x, f,modules=[{"sin":sin , "cos":cos , "tan":tan },math])
-    x0 = 1#float(input("Calc derivative at : "))
-    k = 4#float(input("Order of error magnitude : "))
-    min_h = 0.25#float(input("Set min step size :"))
+    x0 = float(input("Calc derivative at : "))
+    k = float(input("Order of error magnitude : "))
+    min_h = float(input("Set min step size :"))
     derivative = Richardson(f, x0 , min_h , k)
-
     print("The result by Richardson Extrapolation:",derivative)
 
 main()
